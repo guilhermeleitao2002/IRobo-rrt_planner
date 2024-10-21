7ba41babbba4b342c55f84547f9b9b4a3a881b94
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2024-10-21 09:43:08 -0700

    fix: not missing

--- src/rrt_planner.cpp
+++ src/rrt_planner.cpp
@@ -99,7 +99,7 @@ namespace rrt_planner {
                 rand_point_[0] = random_double_x.generate();
                 rand_point_[1] = random_double_y.generate();
 
-            } while(collision_dect_.inFreeSpace(rand_point_));
+            } while(!collision_dect_.inFreeSpace(rand_point_));
         }
 
         return rand_point_;
